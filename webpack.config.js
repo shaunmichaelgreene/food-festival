b601306231557496f485c758bbfc272831cc34bd
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-08-03 18:25:54 -0400

    end 19.3.5, new entry points added and bundled

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,16 @@ const path = require("path");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-  entry: "./assets/js/script.js",
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
+  },
   output: {
-    path: path.join(__dirname + "/dist"),
-    filename: "main.bundle.js"
+    path: __dirname + "/dist",
+    //WAS path: path.join(__dirname + "/dist"), 
+    filename: "[name].bundle.js"
   },
   plugins: [
   new webpack.ProvidePlugin({
